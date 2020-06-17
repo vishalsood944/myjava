566237a5d323bbcf2614ba9a6d385d4660af6a03
Dell <Dell@DESKTOP-AJTSR85>
2020-06-18 02:21:46 +0530

    I'm pussing code first time

---
+++ Loops/src/assignments/swapping1.java
@@ -0,0 +1,28 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class swapping1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int a, b ,c;
+		System.out.println("A value");
+		a=s.nextInt();
+		System.out.println("b value");
+		b=s.nextInt();
+		c=b;
+		b=a;
+		a=c;
+			
+		System.out.println("see the result"+ a);
+		System.out.println("see the result"+ b);
+		System.out.println("swapping 2 variables with using 3rd variable");
+		
+				
+		
+				
+	}
+
+}
---
+++ Loops/src/assignments/swapping2.java
@@ -0,0 +1,19 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class swapping2 {
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int a, b;
+		System.out.println("see first result");
+		a=s.nextInt();
+		System.out.println("see 2nd result");
+		b=s.nextInt();
+		a=b;
+		b=a;
+		System.out.println("swapping 2 variables without using 3rd variable");
+	}
+
+}
